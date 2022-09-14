99c3b60f9e5534eb12a6389227eea36d22b46bd2
broeg <58056914+broeg-spc@users.noreply.github.com>
2022-09-14 16:51:37 +0800

    js jq基础学习

---
+++ chichiTest/js/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: zhangziyang
+ * @Date: 2022-09-07 13:48:57
+ * @LastEditors: git config user.name 
+ * @LastEditTime: 2022-09-07 13:50:11
+ * @FilePath: \emr.webuid:\code\chichiTest\js\index.js
+ * @Description: 
+ */window.AbortController();
